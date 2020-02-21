92049397547d64a64cb8085c89e54d0a7a833074
Ian Thomson <it18aaa@herts.ac.uk>
2020-02-21 11:13:54 +0000

    initial commit

---
+++ app/app.js
@@ -0,0 +1,89 @@
+const scraper = require('./scraper');
+const notifier = require('node-notifier');
+const path = require('path');
+require('dotenv').config();
+
+// use the scraper module to login to the visiontime page
+// and scrape the details
+
+// load config from .env
+var configuration = {
+    badge: process.env.BADGE,
+    pin: process.env.PIN,
+    interval: process.env.INTERVAL,
+    url: process.env.URL,
+    hourStart: process.env.HOUR_START,
+    hourEnd: process.env.HOUR_END
+};
+
+// send config to scheduler
+schedule(configuration);
+
+// scheduler, check every config.interval minutes
+function schedule(config) {
+    checkVisionTime(config)
+        .then(() => {
+            console.log(`Waiting ${config.interval} minutes`);
+            setTimeout(() => {
+                schedule(config);
+            }, 1000 * 60 * config.interval)
+        })
+        .catch(err => console.error('error in scheduler', err));
+};
+
+// get process 
+async function checkVisionTime(config) {
+
+    var hour = new Date().getHours();
+
+    // if we're in normal hours
+    if (hour >= config.hourStart && hour <= config.hourEnd) {
+        scraper.getStatus({ badge: config.badge, pin: config.pin }, config.url)
+            .then(data => {
+                const status = {};
+                // extract the status
+
+                // TODO: use more appropriate method
+                data.map(e => {
+                    if (e === 'Clocked In') {
+                        status.loggedIn = true;
+                    } else if (e === 'Clocked Out') {
+                        status.loggedIn = false;
+                    }
+                })
+
+                // act on the status
+
+                if (status.loggedIn) {
+                    // TODO: turn tray icon green
+                    console.log('You are logged in.')
+                } else {
+                    //TODO: turn tray icon red
+                    notifier.notify({
+                        title: 'VisionTime Alert',
+                        message: 'You are not signed in!',
+                        icon: path.join(__dirname, '../icons/alert.png'),
+                        sound: true
+                    })
+                }
+            })
+            .catch(err => {
+                console.log('there was an error!: ' + err);
+                notifier.notify({
+                    title: 'Oops! Something went awry',
+                    message: 'Error: ' + err
+                });
+                throw err;
+            });
+
+    } else {
+        console.log(`Outside hours:  
+                    This Hour: ${hour}  
+                    Start hour: ${config.hourStart}
+                    End hour: ${config.hourEnd}`);
+    }
+
+
+}
+
+
---
+++ app/scraper.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+async function getStatus(credentials, url) {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    await page.type('#txtBadge', credentials.badge);
+    await page.type('#txtPin', credentials.pin);
+
+    // wrap the 'click and wait' in a promise to prevent deadlock
+    await Promise.all([
+        page.click('#btnClocking'),
+        page.waitForNavigation()
+    ]);
+
+    const data = await page.evaluate(() => {
+        const tds = Array.from(document.querySelectorAll('.label'));
+        return tds.map(td => td.innerText);
+    });
+
+    await browser.close();
+    return data;
+}
+ 
+module.exports = {
+    getStatus: getStatus
+}
